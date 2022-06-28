af3e8c241b4704b1795b52fdfab4ad9d10865ef6
Abrar Alolyan <abrar_ol@hotmail.com>
2022-06-28 11:24:19 +0300

    init

---
+++ src/components/Viggie.jsx
@@ -0,0 +1,8 @@
+
+export default function Viggie() {
+  return (
+    <div>
+     Viggie 
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Popular from "../components/Popular"
+import Viggie from "../components/Viggie"
+
+export default function Home() {
+  return (
+    <>
+        <Popular/>
+        <Viggie />
+    </>
+  )
+}
